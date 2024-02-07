95089793021b916e3c2e710827a6baa39695c382
Deepakkumarrp <deepakkumarrp@gmail.com>
2024-02-07 13:28:35 +0530

    Sprint3Day2

--- sprint3/day2/index.js
+++ sprint3/day2/index.js
@@ -3,14 +3,41 @@ const app = express();
 require("dotenv").config();
 const {connection} = require("./config/db");
 const { userRouter } = require("./routes/user.routes");
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
+
 
 app.use(express.json());
+app.use(cors());
 app.use("/users",userRouter);
 
 
 
 app.get("/",(req,res) => {
     res.send({"msg":"This is a Home Route."})
+})
+
+app.get("/movies",(req,res) => {
+    const {token} = req.query;
+    jwt.verify(token, 'shhhhh',(err,decoded) =>{
+        if(decoded){
+            console.log(decoded);
+            res.send({"mssg":"Movies data.."})
+            
+        }else{
+            res.send({"mssg":"You're not authorized."})
+
+        }
+    });
+    // if(token == "abc123"){
+    //     res.send({"mssg":"Movies data.."})
+    // }else{
+    //     res.send({"mssg":"You're not authorized."})
+    // }
+})
+
+app.get("/series",(req,res) => {
+    res.send({"mssg":"Series data.."})
 })
 
 app.listen(process.env.port,async()=>{
--- sprint3/day2/routes/user.routes.js
+++ sprint3/day2/routes/user.routes.js
@@ -1,26 +1,48 @@
 const express = require("express");
-const {UserModel} = require("../model/user.model")
+const {UserModel} = require("../model/user.model");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken")
 
 const userRouter = express.Router();
 
 userRouter.post("/register",async (req,res) => {
-    const payload = req.body;
+    const {username,email,pass} = req.body;
     try{
-        const user = new UserModel(req.body);
-        await user.save();
-        res.send({"msg":"new user Registered","newUser":req.body})
+        // bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+        //     // Store hash in your password DB.
+        // });
+
+        bcrypt.hash(pass, 8, async function(err, hash) {
+            if(err){
+                res.send({"err":err})
+            }else{
+                const user = new UserModel({username,email,pass:hash});
+                await user.save();
+                res.send({"msg":"new user Registered"})
+
+            }
+        });
     }catch(e){
         res.send({"error":e})
     }
 })
 
 userRouter.post("/login",async(req,res) => {
+    // bcrypt.compare(myPlaintextPassword, hash).then(function(result) {
+        // result == true
+    // });
     const {email,pass} = req.body;
     try{
-        const user = UserModel.findOne({email,pass});
-        if(user){
-            
-        }
+        const user = await UserModel.findOne({email});
+        bcrypt.compare(pass, user.pass).then(function(result) {
+            if( result){
+                res.send({"mssg":"Login Successfully!","token":jwt.sign({ foo: 'bar' }, 'shhhhh')})
+
+            }else{
+                res.send({"msg":"User doesn't exists."})
+
+            }
+        });
     }catch(e){
         res.send({"err":e})
     }
